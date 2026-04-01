7fddde117c3218860affb8d2b1a3fa5cc7178278
Rabie-khashaba <khashabarabie@gmail.com>
2026-04-01 16:00:41 +0200

    add updates

--- app/Http/Controllers/Api/AppUserSocialAuthController.php
+++ app/Http/Controllers/Api/AppUserSocialAuthController.php
@@ -45,7 +45,6 @@ class AppUserSocialAuthController extends Controller
 
             $appRedirect = $this->appRedirectUrl($provider, $result);
 
-           dd(config('services.apple.redirect'));
 
             if ($appRedirect) {
                 return redirect()->away($appRedirect);
